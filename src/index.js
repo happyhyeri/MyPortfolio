3e26ebc2b525cd7b9b0f24608304c9e1743120ed
happyhyeri <hyeri2007@naver.com>
2025-09-03 14:23:53 +0900

    deploy

--- src/index.js
+++ src/index.js
@@ -6,13 +6,14 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode >
+  
      <Provider store={store}>
       <App />
     </Provider>
+  
   </React.StrictMode>
 );
 
